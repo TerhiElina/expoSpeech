3be968d70929798c9c0416d34f90f04beb75e0e3
TerhiElina <terhi.evakallio@gmail.com>
2024-03-22 08:41:32 +0200

    text to speech

--- App.js
+++ App.js
@@ -1,10 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,View, TextInput, Button, Alert } from 'react-native';
+import * as Speech from 'expo-speech';
+import { useState, useEffect } from 'react';
 
 export default function App() {
+  const [text, setText] = useState('');
+  const [voices, setVoices] = useState([]);
+
+  useEffect(() =>{
+    async function fetchVoices(){
+      const availableVoices = await Speech.getAvailableVoicesAsync();
+      setVoices(availableVoices);
+    }
+    fetchVoices();
+  }, []);
+
+
+  const speak = async () => {
+    if(text.length <= Speech.maxSpeechInputLength)
+    {
+    await Speech.speak(text, {
+      language:'fi',
+      voice: voices[0]?.identifier,
+    });
+    } else {
+      console.log('Text exceeds maximum')
+    }
+  };
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+      style={styles.input}
+      placeholder='kirjoita mitä mieleen tulee...'
+      onChangeText={setText}
+      value={text}
+      />
+      <View style={styles.buttonContainer}>
+        <Button
+        title='Kuuntele teksti'
+        onPress={speak}/>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +51,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  
   },
+  input:{
+    borderColor: 'lightgreen',
+    borderWidth: 2,
+    width: 300,
+    height: 300,
+    marginTop: 50,
+    marginLeft: 30,
+    textAlignVertical: 'top',
+
+  },
+  buttonContainer: {
+    marginTop: 30,
+    width: 200,
+    marginLeft: 30,
+  }
 });
